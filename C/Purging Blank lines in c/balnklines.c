b38df6cdde4ddf253a083e657cf196382394b980
gangapurambhargav <gangapurambhargav1@gmail.com>
2022-05-01 11:43:46 +0530

    added blank-lines formats

---
+++ C/Purging Blank lines in c/balnklines.c
@@ -0,0 +1,11 @@
+newline ............ \n
+horizontal tab.... \t
+vertical tab ....... \v
+backspace ........ \b
+carriage return .. \r
+formfeed .......... \f
+audible alert ..... \a
+backslash ......... \\
+question mark .. \?
+single quote ...... \’
+double quote .... \”
